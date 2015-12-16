41c86409e89db494dd83a8c5671c261cf00558a8
Colin Barbour <colinbarbour91@gmail.com>
2015-12-16 12:13:49 +0000

    Adding content Script and button to popup, injecting JS into activeTab.

--- js/popup.js
+++ js/popup.js
@@ -1,21 +1,7 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-
-function click(e) {
-  chrome.tabs.executeScript(null,
-      {code: "alert('hello')"});
-  window.close();
+function hello() {
+  chrome.tabs.executeScript({
+    file: '/js/contentscript.js'
+  }); 
 }
 
-// Setting up the onClick events for each div (aka button)
-// which will trigger the code injection into the active tab.
-
-document.addEventListener('DOMContentLoaded', function () {
-  var divs = document.querySelectorAll('div');
-  console.log(divs);
-  for (var i = 0; i < divs.length; i++) {
-    divs[i].addEventListener('click', click);
-  }
-});
+document.getElementById('forward').addEventListener('click', hello);
