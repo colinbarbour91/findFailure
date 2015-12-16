88d40b18c4b27add36b77cba91d65f50a799ff34
Colin Barbour <colinbarbour91@gmail.com>
2015-12-16 12:59:46 +0000

    Adding new test_get_divs and code stubw

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,1 +1,10 @@
-alert('hello ' + document.location.href);
+//alert('hello ' + document.location.href);
+
+console.log("Starting Content Script");
+
+function getDivs() {
+    /**
+    * Get All divs in the activeTab
+    */
+    return null;
+}
